3d2e012b5b0ddb535b00cbcb0b244c1d4261269e
Litz <thanakrit.p@eth.epson.co.th>
2015-10-16 11:15:34 +0700

    Finish

--- app/src/main/java/epsonlitz/myapplication/MainActivity.java
+++ app/src/main/java/epsonlitz/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package epsonlitz.myapplication;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,8 +24,34 @@ public class MainActivity extends ActionBarActivity {
         //Bind Widget
         bindwidget();
 
+        //Button controller
+
+        buttonController();
 
     } //Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //sound effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.pig);
+                buttonMediaPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/Ay9VW9NzL2c"));
+                startActivity(objIntent);
+
+
+            } //event
+        });
+
+
+    }
+
 
     private void bindwidget() {
 
